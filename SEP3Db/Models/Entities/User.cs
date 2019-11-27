beaf6b49e76a611e30dbf6630db526a367843a8c
Mintiss <43003579+Mintiss@users.noreply.github.com>
2019-11-27 16:33:08 +0100

    Db update
    no id for user

--- SEP3Db/Models/Entities/User.cs
+++ SEP3Db/Models/Entities/User.cs
@@ -9,7 +9,6 @@ namespace SEP3Db.Models.Entities.User
     public class User
     {
         [Key]
-        public int UserId { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public int Type { get; set; }
