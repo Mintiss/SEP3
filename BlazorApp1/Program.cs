9c426fa16b6d86b4447b363f9df487e2a90737f9
kreso11 <43003487+kreso11@users.noreply.github.com>
2019-12-01 19:35:28 +0100

    smal update

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -15,6 +17,19 @@ namespace BlazorApp1
     {
         public static void Main(string[] args)
         {
+            String toSend = "test";
+
+            IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("10.152.220.1"), 2910);
+
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(serverAddress);
+
+            int toSendLen = System.Text.Encoding.ASCII.GetByteCount(toSend);
+            byte[] toSendBytes = System.Text.Encoding.ASCII.GetBytes(toSend);
+            byte[] toSendLenBytes = System.BitConverter.GetBytes(toSendLen);
+            clientSocket.Send(toSendLenBytes);
+            clientSocket.Send(toSendBytes);
+
             CreateHostBuilder(args).Build().Run();
         }
 
---
+++ BlazorApp1/SocketClient/SocketClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace BlazorApp1.SocketClient
+{
+
+    public class SocketClient
+    {
+        public static void main()
+        {
+
+            String toSend = "test";
+
+            IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("localhost"),2910);
+
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(serverAddress);
+
+            int toSendLen = System.Text.Encoding.ASCII.GetByteCount(toSend);
+            byte[] toSendBytes = System.Text.Encoding.ASCII.GetBytes(toSend);
+            byte[] toSendLenBytes = System.BitConverter.GetBytes(toSendLen);
+            clientSocket.Send(toSendLenBytes);
+            clientSocket.Send(toSendBytes);
+
+            clientSocket.Close();
+        }
+    }
+}
