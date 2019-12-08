78ec1a53053c950614d6fa5657f3a4e912e28a42
GretaaJ <43003850+GretaaJ@users.noreply.github.com>
2019-12-08 01:41:30 +0100

    Tier1User

---
+++ Tier1User/Data/Model.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tier1User.Data
+{
+    public class Model
+    {
+        public string email = "123";
+        public string password = "123";
+        public bool isLoggedIn = false;
+
+        public async Task<bool> checkUser(string emailInput, string passwordInput)
+        {
+
+            if (Equals(emailInput, email))
+            {
+                if (Equals(passwordInput, password))
+                {
+                    isLoggedIn = true;
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+
+        }
+
+        public async Task<bool> registerUser(string emailInput, string passwordInput)
+        {
+            return true;
+        }
+
+    }
+}
