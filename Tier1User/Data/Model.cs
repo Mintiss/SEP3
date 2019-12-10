3deef37e3dbcb5d134ec9c84a51c9214dfc17eda
kreso11 <43003487+kreso11@users.noreply.github.com>
2019-12-10 17:49:07 +0100

    commit

--- Tier1User/Data/Model.cs
+++ Tier1User/Data/Model.cs
@@ -1,17 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Tier1User.Networking;
+using Tier1User.Shared;
 
 namespace Tier1User.Data
 {
     public class Model
     {
+        private Service client;
+
+
+        public Model()
+        {
+        }
+
         public string email = "123";
         public string password = "123";
         public bool isLoggedIn = false;
 
+        public bool LogInFalse = false;
+        public bool LogInConfirmed = false;
+
         public async Task<bool> checkUser(string emailInput, string passwordInput)
+        {
+            /*Debug.WriteLine(emailInput, passwordInput);
+
+            User u = new User(emailInput, passwordInput);
+
+            String jsonuser = Newtonsoft.Json.JsonConvert.SerializeObject(u);
+
+            Debug.WriteLine(jsonuser);
+
+            JsonInstruction js = new JsonInstruction(jsonuser, "LoginInfo");
+
+            String jsonall = Newtonsoft.Json.JsonConvert.SerializeObject(js);
+
+            Debug.WriteLine(jsonall);
+*/
+            var gotFromServer=
+
+            System.Threading.Thread.Sleep(500);
+
+
+            if (this.LogInConfirmed == true)
+            {
+                return true;
+            }
+            if (this.LogInFalse == false)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public void logFail()
+        {
+            this.LogInFalse = false;
+        }
+
+        public void LogIn()
+        {
+            this.LogInConfirmed = true;
+        }
+
+        public async Task<bool> checkUserf(string emailInput, string passwordInput)
         {
 
             if (Equals(emailInput, email))
---
+++ Tier1User/Networking/SocketClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Tier1User.Data;
+
+namespace Tier1User.Networking
+{
+    public class Service 
+    
+    {
+        HttpClient client;
+
+        public Service(IHttpClientFactory clientA)
+        {
+            client=clientA.CreateClient();
+        }
+
+        public HttpClient getClient()
+        {
+            return this.client;
+        }
+    }
+}
