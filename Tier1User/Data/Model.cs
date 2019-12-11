5f5ff69d93cf4b174d4fb48707c52c2420617c22
kreso11 <43003487+kreso11@users.noreply.github.com>
2019-12-11 16:13:33 +0100

    login and register working

--- Tier1User/Data/Model.cs
+++ Tier1User/Data/Model.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Tier1User.Networking;
 using Tier1User.Shared;
@@ -19,30 +20,8 @@ namespace Tier1User.Data
             service = new Service();
         }
 
-        public string email = "123";
-        public string password = "123";
-        public bool isLoggedIn = false;
-
-        public bool LogInFalse = false;
-        public bool LogInConfirmed = false;
-
         public async Task<bool> checkUser(string emailInput, string passwordInput)
-        {
-            /*Debug.WriteLine(emailInput, passwordInput);
-
-            User u = new User(emailInput, passwordInput);
-
-            String jsonuser = Newtonsoft.Json.JsonConvert.SerializeObject(u);
-
-            Debug.WriteLine(jsonuser);
-
-            JsonInstruction js = new JsonInstruction(jsonuser, "LoginInfo");
-
-            String jsonall = Newtonsoft.Json.JsonConvert.SerializeObject(js);
-
-            Debug.WriteLine(jsonall);
-*/
-            
+        {            
             var gotFromServer = await service.getClient().GetStringAsync("http://localhost:8543/Users/"+emailInput).ConfigureAwait(false);
 
             var jsonall = Newtonsoft.Json.JsonConvert.DeserializeObject(gotFromServer);
@@ -54,35 +33,20 @@ namespace Tier1User.Data
             return false;
         }
 
-        public void logFail()
-        {
-            this.LogInFalse = false;
-        }
-
-        public void LogIn()
-        {
-            this.LogInConfirmed = true;
-        }
-
-        public async Task<bool> checkUserf(string emailInput, string passwordInput)
-        {
-
-            if (Equals(emailInput, email))
-            {
-                if (Equals(passwordInput, password))
-                {
-                    isLoggedIn = true;
-                    return true;
-                }
-                else return false;
-            }
-            else return false;
-
-        }
-
         public async Task<bool> registerUser(string emailInput, string passwordInput)
         {
+            Debug.WriteLine(emailInput, passwordInput);
+
+            var jsonall = Newtonsoft.Json.JsonConvert.SerializeObject(emailInput+","+passwordInput);
+
+            var stringContent = new StringContent(jsonall, UnicodeEncoding.UTF8, "application/json");
+
+            Debug.WriteLine(stringContent);
+
+            await service.getClient().PostAsync("http://localhost:8543/Users/",stringContent).ConfigureAwait(false);
+
             return true;
+        
         }
 
     }
