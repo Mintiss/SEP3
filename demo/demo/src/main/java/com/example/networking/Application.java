5c4d214d0ecd2b0f756ede0a1265a7c893faded5
kreso11 <43003487+kreso11@users.noreply.github.com>
2019-12-12 13:34:57 +0100

    beforemerge

--- demo/demo/src/main/java/com/example/networking/Application.java
+++ demo/demo/src/main/java/com/example/networking/Application.java
@@ -1,7 +1,6 @@
 package com.example.networking;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.SharedControllers")
+@ComponentScan(basePackages = "com.example.networking")
+
 public class Application {
 
     public static void main(String[] args) {
--- demo/demo/src/main/java/com/example/networking/model/ServerModel.java
+++ demo/demo/src/main/java/com/example/networking/model/ServerModel.java
@@ -42,8 +42,11 @@ public class ServerModel {
         }
     }
 
+    //FIX IT DO LOGIC IN T3
     public void checkUserInfoOnLogin(User user){
+
         User userGotFromDB =uc.getUserFromDB(user.getUsername());
+
         if (userGotFromDB.getUsername()==null)
         {
             System.out.println("null");
@@ -61,6 +64,7 @@ public class ServerModel {
             else
                 support.firePropertyChange("LogInFailed",null,null);
         }
+
     }
 
     public void UpdateMainTable() {
